3eed0ee505ebfaa9db212c11c0681c394fa91e59
lanshayu <fingnet@gmail.com>
2014-10-27 17:18:27 +0800

    编辑器控件增加全屏按钮

--- application/view/form.js
+++ application/view/form.js
@@ -49,7 +49,7 @@ define(function(require){
 						items : [
 							'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic', 'underline',
 							'removeformat', '|', 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist',
-							'insertunorderedlist', '|', 'emoticons', 'image', 'link']
+							'insertunorderedlist', '|', 'image', 'link', '|', 'fullscreen']
 					});
 				},500);
 			}
