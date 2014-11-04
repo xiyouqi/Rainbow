c5b934c0f36e1ce6d840d41797aa4545bfdba214
lanshayu <fingnet@gmail.com>
2014-11-04 14:54:03 +0800

    通知列表无法现实bug修复

--- application/common/notice.js
+++ application/common/notice.js
@@ -50,6 +50,7 @@ define(function(require){
 				        if(response.length > 0){
 				        	 $notice.append('<span class="badge badge-important">' + response.length + '</span>');
 				        	 response.length > messageNum && playSound();
+				        	 messages = response;
 				        }
 				        messageNum = response.length;
 				    }
